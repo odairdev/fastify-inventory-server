f392ee5786d3207c8ce0081bdd32451a0d54817f
unknown <odair.dev@gmail.com>
2023-04-25 13:14:39 -0300

    Error hadler added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import fastify from "fastify";
 import { UsersRoutes } from "./routes/users";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 const app = fastify()
 
 app.register(UsersRoutes, {
   prefix: '/users'
+})
+
+app.setErrorHandler((error, request, reply) => {
+  if(error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({
+        message: 'Validation Error',
+        issues: error.format()
+      })
+  }
+
+  if(env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({message: 'Internal server error.'})
 })
 
 export { app }
